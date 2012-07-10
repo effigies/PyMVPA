6119a7daa9c9f7ec5ce440d766d613b76737d568
Chris Johnson <effigies@bu.edu>
2012-07-10 11:51:00 -0400

    NF Add IndexedQueryEngine-like SurfaceDiskQueryEngine

--- mvpa2/misc/neighborhood.py
+++ mvpa2/misc/neighborhood.py
@@ -7,16 +7,20 @@
 #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """ Neighborhood objects """
+#from __future__ import division
 
 import numpy as np
 from numpy import array
 import sys
 import itertools
+import nibabel as nib
+import networkx as nx
 
 from mvpa2.base import warning
 from mvpa2.base.types import is_sequence_type
 from mvpa2.base.dochelpers import borrowkwargs, borrowdoc, _repr_attrs, _repr
 from mvpa2.clfs.distance import cartesian_distance
+from mvpa2.datasets.mri import fmri_dataset
 
 from mvpa2.misc.support import idhash as idhash_
 
@@ -339,6 +343,107 @@ class HollowSphere(Sphere):
         if not len(res):
             warning("%s defines no neighbors" % self)
         return np.vstack([np.zeros(ndim,dtype='int'),res]) if self.include_center else res
+
+
+class SurfaceDiskQueryEngine(object):
+    """Disk on a surface embedded in a higher dimensional space
+
+    This is an IndexedQueryEngine, in effect, but requires:
+    1) NifTI images of left and right hemispheres, with nearest neighbor
+    vertex indices for each voxel as the values
+    2) Left and right hemisphere graphs which are lists of dictionaries
+    such that G[i][j] is the distance between connected nodes i and j
+    3) Coordinates of vertices in Euclidean space
+    """
+    def __init__(self, radius, lverts, lgraph, lcoords,
+                               rverts, rgraph, rcoords):
+        self._radius = radius
+        self.lverts = lverts
+        self.rverts = rverts
+        self.lgraph = lgraph
+        self.rgraph = rgraph
+        self.lcoords = lcoords
+        self.rcoords = rcoords
+
+        self.lvox = [set(np.nonzero(lv.samples[0] != -1)[0].tolist())
+                     for lv in lverts]
+        self.rvox = [set(np.nonzero(rv.samples[0] != -1)[0].tolist())
+                     for rv in rverts]
+        self.lvoxall = set.union(*self.lvox)
+        self.rvoxall = set.union(*self.rvox)
+
+    def __repr__(self, prefixes=[]):
+        return "%s(%s)" % (self.__class__.__name__, ', '.join(prefixes))
+
+    # Cargo cult programming
+    @property
+    def radius(self):
+        return self._radius
+
+    @classmethod
+    def loadFromFiles(cls, radius, lvtxvols, lhsurf, rvtxvols, rhsurf,
+            mask=None):
+
+        if isinstance(lvtxvols, basestring):
+            lvtxvols = [lvtxvols]
+        if isinstance(rvtxvols, basestring):
+            rvtxvols = [rvtxvols]
+
+        # Vertex volumes generated with Freesurfer's mri_surf2vol --vtxvol
+        lverts = [fmri_dataset(vol, mask=mask) for vol in lvtxvols]
+        rverts = [fmri_dataset(vol, mask=mask) for vol in rvtxvols]
+
+        # Standard freesurfer [lr]h.* files
+        lcoords, lfaces = nib.freesurfer.read_geometry(lhsurf)
+        rcoords, rfaces = nib.freesurfer.read_geometry(rhsurf)
+
+        def buildGraph(coords, faces):
+            """Build a bidirectional graph with Euclidean distances as edges"""
+            graph = nx.Graph()
+
+            euclid = lambda x, y: np.sqrt(np.sum((x - y) ** 2))
+            for i, j, k in faces:
+                graph.add_edge(i, j, weight=euclid(coords[i, :], coords[j, :]))
+                graph.add_edge(i, k, weight=euclid(coords[i, :], coords[k, :]))
+                graph.add_edge(j, k, weight=euclid(coords[j, :], coords[k, :]))
+
+            return graph
+
+        lgraph = buildGraph(lcoords, lfaces)
+        rgraph = buildGraph(rcoords, rfaces)
+
+        return cls(radius, lverts, lgraph, lcoords, rverts, rgraph, rcoords)
+
+    def train(self, dataset):
+        self.ids = np.arange(dataset.nfeatures)
+
+    def __getitem__(self, coordinate):
+        if coordinate in self.lvoxall:
+            graph = self.lgraph
+            vertices = self.lverts
+            for verts, voxels in zip(self.lverts, self.lvox):
+                if coordinate in voxels:
+                    source = verts.samples[0, coordinate]
+                    break
+        elif coordinate in self.rvoxall:
+            graph = self.rgraph
+            vertices = self.rverts
+            for verts, voxels in zip(self.rverts, self.rvox):
+                if coordinate in voxels:
+                    source = verts.samples[0, coordinate]
+                    break
+        else:
+            # XXX BIG ASSUMPTION
+            # Index 0 will never be informative
+            return [0]
+
+        disk = nx.single_source_dijkstra_path_length(graph, source,
+                                                     self._radius)
+
+        idcs = set.union(*[set(i for i, v in enumerate(vert.samples[0])
+                               if v in disk) for vert in vertices])
+
+        return sorted(idcs)
 
 
 class QueryEngineInterface(object):
